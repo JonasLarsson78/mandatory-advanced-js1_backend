d1d56435f1ef24451dae858928302a20440a4d42
JonasLarsson78 <jl.7804@gmail.com>
2020-06-05 16:44:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const port = 3010;
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    console.log(message);
+    const id = uuidv4();
+    const date = new Date();
+    const newMessage = { ...message, id, date };
+    io.sockets.emit('new_message', newMessage);
+  });
+  console.log('New User connected');
+});
+server.listen(port, () => console.log(`App listening on port ${port}!`));
