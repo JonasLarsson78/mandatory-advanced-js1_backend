56c539263c874ae96a6bab44fcf1feed780bf0eb
JonasLarsson78 <jl.7804@gmail.com>
2020-06-08 19:55:58 +0200

    fix

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 const INDEX = '/index.html';
 
 const server = express()
+  .use(cors())
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
