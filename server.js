568d18f9ed092ad57e7244a16c58160e3b5e0b65
JonasLarsson78 <jl.7804@gmail.com>
2020-06-08 21:55:42 +0200

    fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const socketIO = require('socket.io');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -15,6 +16,16 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+});
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    console.log(message);
+    const id = uuidv4();
+    const date = new Date();
+    const newMessage = { ...message, id, date };
+    io.sockets.emit('new_message', newMessage);
+  });
+  console.log('New User connected');
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
